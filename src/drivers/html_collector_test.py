26684aa3620ec63f24f2389668639770d72c2ed8
Tonho <antonioreverso.com@gmail.com>
2025-04-29 19:39:07 -0300

    correção

--- src/drivers/html_collector_test.py
+++ src/drivers/html_collector_test.py
@@ -6,5 +6,8 @@ def test_collect_essential_information():
 
     html_collector = HtmlCollector()
     essential_information = html_collector.collect_essential_information(http_request_response["html"])
-    print()
-    print(essential_information)
+    
+    assert isinstance(essential_information, list)
+    assert isinstance(essential_information[0], dict)
+    assert 'name' in essential_information[0]
+    assert 'link' in essential_information[0]
--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -4,10 +4,11 @@ import requests
 class HttpRequester:
 
     def __init__(self) -> None:
-        self.__url = "https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm"
+        self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
 
     def request_from_page(self) -> Dict[int, str]:
         response = requests.get(self.__url)
         return {
             "status_code": response.status_code,
-            "html": response.text}             
+            "html": response.text
+        }
